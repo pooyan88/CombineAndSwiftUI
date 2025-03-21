fee72cc4e41cda392684c3105a1ac70d44a57599
Pooyan J <pooyanjavidi@gmail.com>
2025-03-21 09:19:11 +0330

    Initial Commit

---
+++ CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWithSwiftUIApp.swift
+//  CombineWithSwiftUI
+//
+//  Created by Pooyan J on 1/1/1404 AP.
+//
+
+import SwiftUI
+
+@main
+struct CombineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineWithSwiftUI
+//
+//  Created by Pooyan J on 1/1/1404 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
