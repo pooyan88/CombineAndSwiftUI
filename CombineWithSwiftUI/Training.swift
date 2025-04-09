e7a97551c7f8af378cc9b3485d26261cc513cde1
Pooyan J <pooyanjavidi@gmail.com>
2025-04-09 10:23:59 +0330

    Finished TryAllSatisfy and contains training

--- CombineWithSwiftUI/Training.swift
+++ CombineWithSwiftUI/Training.swift
@@ -256,7 +256,54 @@ final class CancelPipeLineViewModel: ObservableObject {
     }
 }
 
+struct ContainsExample: View {
+    @StateObject var vm = ContainsExampleViewModel()
+    var body: some View {
+        VStack {
+            Text(vm.description)
+            Toggle("heating", isOn: $vm.heating)
+            Toggle("air condition", isOn: $vm.airConditioning)
+            Toggle("bathroom", isOn: $vm.basement)
+            Toggle("basement", isOn: $vm.basement)
+        }.padding()
+            .onAppear {
+                vm.fetch()
+            }
+    }
+}
+
+final class ContainsExampleViewModel: ObservableObject {
+
+    @Published var heating: Bool = false
+    @Published var airConditioning: Bool = false
+    @Published var basement: Bool = false
+    @Published var bathroom: Bool = false
+    @Published var description: String = ""
+    var cancellable: AnyCancellable?
+
+    func fetch() {
+        let dataIn: [String] = ["heating", "basement", "airconditioning", "bathroom"]
+        cancellable = dataIn.publisher
+            .prefix(2)
+            .sink { [unowned self] string in
+                description += string + "\n"
+            }
+        dataIn.publisher
+            .contains("heating")
+            .assign(to: &$heating)
+        dataIn.publisher
+            .contains("airconditioning")
+            .assign(to: &$airConditioning)
+        dataIn.publisher
+            .contains("basement")
+            .assign(to: &$basement)
+        dataIn.publisher
+            .contains("bathroom")
+            .assign(to: &$bathroom)
+    }
+}
+
 
 #Preview {
-    CancelPipeLineView()
+    ContainsExample()
 }
