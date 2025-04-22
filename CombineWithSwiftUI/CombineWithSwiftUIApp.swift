5c973ccdd0e6777d4041aac0d6b8b49c6579b768
Pooyan J <pooyanjavidi@gmail.com>
2025-04-22 16:04:41 +0330

    DropFirst

--- CombineWithSwiftUI/CombineWithSwiftUIApp.swift
+++ CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CombineWithSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            DropUntilOutputFromExample()
+            DropFirstExample()
         }
     }
 }
